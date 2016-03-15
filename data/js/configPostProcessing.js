08239a1c962de9086e8f05b5951155f0557741db
DarkVaDehors <johnnybehm@gmail.com>
2016-03-15 02:14:59 +0100

    traduction française

--- data/js/configPostProcessing.js
+++ data/js/configPostProcessing.js
@@ -37,14 +37,14 @@ $(document).ready(function () {
             function (data) {
                 if (data == "invalid") {
                     $('#naming_pattern').qtip('option', {
-                        'content.text': 'This pattern is invalid.',
+                        'content.text': 'Ce modèle est invalide.',
                         'style.classes': 'ui-tooltip-rounded ui-tooltip-shadow ui-tooltip-red'
                     });
                     $('#naming_pattern').qtip('toggle', true);
                     $('#naming_pattern').css('background-color', '#FFDDDD');
                 } else if (data == "seasonfolders") {
                     $('#naming_pattern').qtip('option', {
-                        'content.text': 'This pattern would be invalid without the folders, using it will force "Flatten" off for all shows.',
+                        'content.text': 'Ce modèle ne serait pas valide sans les dossiers, l\'utiliser forcera le mode dossier pour tous les spectacles.',
                         'style.classes': 'ui-tooltip-rounded ui-tooltip-shadow ui-tooltip-red'
                     });
                     $('#naming_pattern').qtip('toggle', true);
@@ -83,14 +83,14 @@ $(document).ready(function () {
             function (data) {
                 if (data == "invalid") {
                     $('#naming_abd_pattern').qtip('option', {
-                        'content.text': 'This pattern is invalid.',
+                        'content.text': 'Ce modèle est invalide.',
                         'style.classes': 'ui-tooltip-rounded ui-tooltip-shadow ui-tooltip-red'
                     });
                     $('#naming_abd_pattern').qtip('toggle', true);
                     $('#naming_abd_pattern').css('background-color', '#FFDDDD');
                 } else if (data == "seasonfolders") {
                     $('#naming_abd_pattern').qtip('option', {
-                        'content.text': 'This pattern would be invalid without the folders, using it will force "Flatten" off for all shows.',
+                        'content.text': 'Ce modèle ne serait pas valide sans les dossiers, l\'utiliser forcera le mode dossier pour tous les spectacles.',
                         'style.classes': 'ui-tooltip-rounded ui-tooltip-shadow ui-tooltip-red'
                     });
                     $('#naming_abd_pattern').qtip('toggle', true);
